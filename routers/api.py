1a1ff0b8b8f2a080f0a834feef7f8d85ec97cf89
Ganesh Motekar <ganesh.motekar@pluraltechnology.com>
2023-08-19 22:17:36 +0530

    If applied, changed api.py file v17

--- routers/api.py
+++ routers/api.py
@@ -31,8 +31,8 @@ from sklearn.model_selection import train_test_split
 import json
 
 # from config import connection_string, azureShareName, userEmailId, nodeAPIURL, spacyCharLimit #,AppDBLogs,  getUserDetails,  getStatusDetails , getContractFieldMapping, getContractFilesCount#,  azureMonitorkey
-from core.database import Base, engine
-from core import models
+# from core.database import Base, engine
+# from core import models
 
 router = APIRouter(tags=['NLP Learning'])
 TRAIN_DATA = []
