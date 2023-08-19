fb0c8028265a4601b65bb850a42418858296f359
Ganesh Motekar <ganesh.motekar@pluraltechnology.com>
2023-08-19 23:42:55 +0530

    If applied, changed api.py file v19

--- core/database.py
+++ core/database.py
@@ -2,7 +2,67 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import urllib
-from config import AppAzureExtractionDBLogs
+
+
+def AppAzureExtractionDBLogs(logMessages: str):
+    try:
+        from azure.storage.fileshare import ShareClient, ShareFileClient, ShareDirectoryClient
+        from datetime import datetime
+        import os, shutil, re
+        from azure.core.exceptions import ResourceExistsError
+        import ssl
+
+        dt = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+        
+        # from sqlalchemy import select
+        # db = Session(bind=engine, expire_on_commit=False)
+        # model_name = db.query(models.contracts).filter(models.contracts.id==contractID).first()
+        # print("modelname- ", model_name.displayModelName)        
+        # db.close()
+        model_name = "databaselogs"
+        my_modelname = re.sub('[^a-zA-Z0-9 \n\.]', '', model_name.displayModelName)
+        modelname = my_modelname.replace(" ", "_").lower()  
+        filename = dt
+
+        with open(filename, mode='w') as log_file:
+            content = f"{logMessages}"
+            log_file.write('\n')
+            log_file.write(content) 
+
+            current_directory = os.getcwd()
+            rm_dirPath = os.path.join(current_directory)
+            share = ShareClient.from_connection_string(connection_string, share_name=azureShareName)
+            filesdir = []
+            filesdir.append(os.path.join(filename))
+            
+            try:
+                NLPModel = share.get_directory_client("nlp-database")
+                NLPModel.create_directory()
+            except ResourceExistsError:
+                pass
+            except ssl.CertificateError:
+                pass
+            try:
+                NLPModel_ProjectName = share.get_directory_client("nlp-database/"+modelname)
+                NLPModel_ProjectName.create_directory()
+            except ResourceExistsError:
+                pass
+            except ssl.CertificateError:
+                pass 
+            properties = share.get_share_properties()
+            
+            file = ShareFileClient.from_connection_string(connection_string,share_name=azureShareName +"/nlp-database/"+modelname,file_path=filename)
+            
+            filepath = current_directory +"/"+filename
+
+            with open(filepath, "rb") as source_file:
+                file.upload_file(source_file)            
+            print("uploaded")  
+
+    except Exception as ex:
+        print(f"Azure log file upload error -- {ex}")
+        return "Error", ex
+
 
 
 try:
